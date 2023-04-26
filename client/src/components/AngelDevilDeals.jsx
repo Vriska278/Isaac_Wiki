3ca5ff108aebb37f101b976fc4c1c2098c5ca125
xSutera <710ctl@gmail.com>
2023-04-26 12:31:44 -0400

    paragraph

--- client/src/components/AngelDevilDeals.jsx
+++ client/src/components/AngelDevilDeals.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function AngelDevilDeals() {
-  return <div>AngelDevilDeals</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="card shadow">
+          <div className="card-header d-flex justify-content-center ">
+            <h1> Angel & Devil Deals </h1>
+          </div>
+          <div className="card-body">
+            <div className="d-flex justify-content-center">
+              <p>
+                Devil rooms typically only have one percent chance to spawn on a
+                floor. There are a few ways to increase these odds. One of those
+                ways it to take no red heart damage on the current floor. This
+                will make the percent chance go up to 100 percent. Another
+                option is taking no red heart damage during a boss fight, this
+                increases the chance to get a devil deal by 35 percent. Having
+                certain items also increase the percentage chance of a devil
+                deal appearing. These items include the pentagram, Book of
+                revelations, goat head, the book of belilal, black candle,
+                satanic bible wisps. Soul hearts
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AngelDevilDeals;
