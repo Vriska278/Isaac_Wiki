70a54ed43195bdd55abccaff2a42be14e2da5f55
xSutera <710ctl@gmail.com>
2023-04-27 15:32:36 -0400

    mechanics and tips

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import {Navigate, Route, Routes} from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import BossList from './components/BossList';
 import Navbar from './components/Navbar';
 import BossDetails from './components/BossDetails';
-import AngelDevilDeals from './components/AngelDevilDeals'
+import AngelDevilDeals from './components/AngelDevilDeals';
+import AdvancedMechanics from './components/AdvancedMechanics';
 
 function App() {
   return (
     <>
-    <Navbar />
-      <div className="container">
+      <Navbar />
+      <div className='container'>
         <Routes>
           <Route path='/' element={<Navigate to='/bosses' />} />
           <Route path='/bosses' element={<BossList />} />
           <Route path='/bosses/:id' element={<BossDetails />} />
           <Route path='/deals' element={<AngelDevilDeals />} />
+          <Route path='/mechanics' element={<AdvancedMechanics />} />
         </Routes>
       </div>
     </>
--- client/src/components/AngelDevilDeals.jsx
+++ client/src/components/AngelDevilDeals.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../css/AngelDevilDeals.module.css';
 
 function AngelDevilDeals() {
   return (
@@ -9,25 +10,27 @@ function AngelDevilDeals() {
             <h1> Angel & Devil Deals </h1>
           </div>
           <div className='card-body'>
-            <div className='d-flex justify-content-center'>
-              <p>
-                Devil rooms at the start of the every floor past the first floor
-                have a 100 percent chance of spawning. One of those ways it to
-                take no red heart damage on the current floor. Another option is
-                taking no red heart damage during a boss fight, this increases
-                the chance to get a devil deal by 35 percent. Having certain
-                items also increase the percentage chance of a devil deal
-                appearing. These items include the pentagram, Book of
-                revelations, goat head, the book of belilal, black candle,
-                satanic bible wisps. Soul hearts and black hearts protect your
-                chance of a devil deals if damage is taken. If a devil deal is
-                taken, only devil deals will occur for the rest of the run. For
-                Angel Rooms to appear two conditions must be met. The first
-                condition is a devil must has spawned on a previous floor. The
-                second condition is no devil deal made up until that point in
-                the run.
-              </p>
-            </div>
+            <p>
+              Devil rooms at the start of the every floor past the first floor
+              have a 100 percent chance of spawning. One of those ways it to
+              take no red heart damage on the current floor. Another option is
+              taking no red heart damage during a boss fight, this increases the
+              chance to get a devil deal by 35 percent.
+            </p>
+            <p>
+              Having certain items also increase the percentage chance of a
+              devil deal appearing. These items include the pentagram, Book of
+              revelations, goat head, the book of belilal, black candle, satanic
+              bible wisps.
+            </p>
+            <p>
+              Soul hearts and black hearts protect your chance of a devil deals
+              if damage is taken. If a devil deal is taken, only devil deals
+              will occur for the rest of the run. For Angel Rooms to appear two
+              conditions must be met. The first condition is a devil must has
+              spawned on a previous floor. The second condition is no devil deal
+              made up until that point in the run.
+            </p>
           </div>
         </div>
       </div>
